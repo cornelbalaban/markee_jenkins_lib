739102abaa29ea60baa48b919cec986ddad2a998
cornelbalaban <cornel.balaban@gmail.com>
2021-03-09 23:12:46 +0200

    correct import

--- src/com/markee/jenkins_lib/GitTool.groovy
+++ src/com/markee/jenkins_lib/GitTool.groovy
@@ -3,8 +3,7 @@ package com.markee.jenkins_lib
 class GitTool {
 
     def pullFromGit() {
-        sh "echo Pulling from GIT"
-
+        
     }
 
 }
--- vars/gitStep.groovy
+++ vars/gitStep.groovy
@@ -1,10 +1,5 @@
 #! /usr/bin/env groovy
 
-import com.markee.jenkins_lib.GitTool
-
 def call(String stepName) {
-    sh "echo ${stepName}}"
-
-    def gitTool = new GitTool()
-    gitTool.pullFromGit()
+    echo "Starting " + stepName
 }
