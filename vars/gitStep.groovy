ae0656306aca6d4eba30de76b1b368d9f2cb9d13
cornelbalaban <cornel.balaban@gmail.com>
2021-03-09 22:46:58 +0200

    new script method

--- vars/gitStep.groovy
+++ vars/gitStep.groovy
@@ -1,5 +1,18 @@
 #! /usr/bin/env groovy
 
-def call(String stepName) {
+def call(String stepName, Map options) {
     echo "Starting " + stepName
+
+    pullFromGit(options, options.repoUrl)
+}
+
+
+def pullFromGit(Map credentials,String repoUrl) {
+    echo "Pulling from GIT"
+
+    if (credentials.userName == null) {
+        echo "No user or pwd for private repo"
+        error("Aborting build")
+    }
+
 }
