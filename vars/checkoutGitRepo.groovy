6ac753e968514323d9f699094ecc8da2a275d6af
cornelbalaban <cornel.balaban@gmail.com>
2021-03-11 23:36:55 +0200

    wrapper add

--- vars/checkoutGitRepo.groovy
+++ vars/checkoutGitRepo.groovy
@@ -1,10 +1,15 @@
 def call() {
     echo "Checking out git repo"
 
+    script.withEnv {
+        (["test=blah"])
+    }
+
     try {
         sh "gradle build"
     } catch(Exception e) {
       echo "Exception ${e.localizedMessage}"
+        echo "$test"
     }
 
 }
